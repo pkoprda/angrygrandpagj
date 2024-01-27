42682f596aec95ef42989ae00bbc45cb3cb00a58
Remi Laramas <remi.laramas@grenoble-inp.org>
2024-01-27 15:16:55 +0200

    Fully rotating character

--- Scenes/Test/Rotation.gd
+++ Scenes/Test/Rotation.gd
@@ -1,13 +1,15 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var p_dir = get_parent().velocity
 	if p_dir != Vector3.ZERO:
-		look_at(get_parent().global_position-p_dir)
+		if get_parent().grabbed_object != null :
+			var ve = get_parent().grabbed_object.global_position - get_parent().global_position
+			look_at(get_parent().global_position - ve)
+		else : 
+			look_at(get_parent().global_position-p_dir)
